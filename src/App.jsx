4194163ade7d78205b907b46ee883227aa484e88
visitvb <rathishkuthirummal@gmail.com>
2026-04-03 13:49:28 +0530

    feat: configure react router with home and about routes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+// We will create a Navbar component later
+// import Navbar from './components/Navbar'; 
+
+function App() {
+  return (
+    <Router>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
